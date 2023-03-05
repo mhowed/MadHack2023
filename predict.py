72d2914d5fe85a8506943807055a2c002c54f722
Matt <mhowed@gmail.com>
2023-03-04 23:20:59 -0600

    added checkers to make sure first datapoint good

--- predict.py
+++ predict.py
@@ -5,12 +5,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
-from polygon import RESTClient
-from polygon.rest import models
 import pandas as pd
 import yfinance as yahoo
 import os.path
-import json
 
 
 def prediction(ticker):
@@ -19,30 +16,32 @@ def prediction(ticker):
     df = None
     if not os.path.exists("data"):
         os.mkdir("data")
-        
+
     if os.path.isfile(f"data/{ticker}.csv"):
-        dateOfFile = datetime.fromtimestamp(os.path.getmtime(f"data/{ticker}.csv")).strftime('%Y-%m-%d')
-        
+        dateOfFile = datetime.fromtimestamp(os.path.getmtime(
+            f"data/{ticker}.csv")).strftime('%Y-%m-%d')
+
         if dateOfFile < currtime:
             print("Fetching new Data")
-            pd.DataFrame(yahoo.download(ticker, "2012-01-01", currtime)).to_csv(f"data/{ticker}.csv")
+            pd.DataFrame(yahoo.download(ticker, "2012-01-01",
+                         currtime)).to_csv(f"data/{ticker}.csv")
     else:
         print("Getting new Data")
-        pd.DataFrame(yahoo.download(ticker, "2012-01-01", currtime)).to_csv(f"data/{ticker}.csv")
+        pd.DataFrame(yahoo.download(ticker, "2012-01-01", currtime)
+                     ).to_csv(f"data/{ticker}.csv")
 
     df = pd.read_csv(f"data/{ticker}.csv")
 
-    plt.style.use("dark_background")
+    plt.style.use("seaborn-v0_8-bright")
 
     data = df.filter(["Close"])
     dataset = data.values
-
 
     train_split = .80
 
     train_len = math.ceil(len(dataset) * train_split)
 
-    scaler = MinMaxScaler(feature_range=(0,1))
+    scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(dataset)
 
     train_data = scaled_data[0:train_len, :]
@@ -54,68 +53,91 @@ def prediction(ticker):
 
     for i in range(days, len(train_data)):
         x_train.append(train_data[i-days:i, 0])
-        y_train.append(train_data[i,0])
-            
+        y_train.append(train_data[i, 0])
+
     x_train, y_train = np.array(x_train), np.array(y_train)
+
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
     model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1],1)))
+    model.add(LSTM(50, return_sequences=True,
+              input_shape=(x_train.shape[1], 1)))
     model.add(LSTM(50, return_sequences=False))
     model.add(Dense(25))
-    #model.add(Dense(25))
+    # model.add(Dense(25))
     model.add(Dense(1))
 
-    #compile
+    # compile
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    #fit using x,y,every piece of data,only 1 run
+    # fit using x,y,every piece of data,only 1 run
     model.fit(x_train, y_train, batch_size=1, epochs=1)
 
-    test = scaled_data[train_len - days: , :]
-    #Create the data set x_test and y_test
+    test = scaled_data[train_len - days:, :]
+    # Create the data set x_test and y_test
     x_test = []
     y_test = dataset[train_len:, :]
     for i in range(days, len(test)):
         x_test.append(test[i-days:i, 0])
 
-    #make it a numpy array
+    # make it a numpy array
     x_test = np.array(x_test)
 
-    #reshape data
+    # reshape data
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-    #Get the models predicted price values
+    # Get the models predicted price values
     predictions = model.predict(x_test)
     predictions = scaler.inverse_transform(predictions)
 
-
-    #Get the root mean squared error (RMSE)
+    # Get the root mean squared error (RMSE)
     rmse = np.sqrt(np.mean(predictions - y_test)**2)
     rmse
     print(rmse)
 
-    #get data we didnt touch
+    # get data we didnt touch
     valid = data[train_len:]
-    #data that was predicted
+    # data that was predicted
     train = data[:train_len]
     valid['Predictions'] = predictions
 
-    #plot data
-    plt.figure(figsize=(16,8))
+    return train, valid, predictions, rmse
+
+
+def plot(train, valid, predictions):
+    # plot data
+    plt.figure(figsize=(16, 8))
     plt.title('Model')
     plt.xlabel('Date')
     plt.ylabel('Close Price')
     plt.plot(train['Close'])
     plt.plot(valid[['Close', 'Predictions']])
-    plt.legend(['Train', 'Val', 'Predictions'], loc='upper left')
+    plt.legend(['Train', 'Real', 'Predictions'], loc='upper left')
     plt.savefig('image.png')
 
     print(predictions[-1])
 
-    print(valid)
 
 def main():
-    prediction('AAPL')
+    pd.options.mode.chained_assignment = None
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    train, valid, predictions, rmse = prediction('AAPL')
+
+    # make sure good start
+    x = valid.head(1)
+    real = x["Close"].values
+    pred = x["Predictions"].values
+    print(real/pred)
+
+    while real/pred > 1.02 or real/pred < 0.98:
+        train, valid, predictions, rmse = prediction('AAPL')
+        x = valid.head(1)
+        real = x["Close"].values
+        pred = x["Predictions"].values
+        print(real/pred)
+
+    plot(train, valid, predictions)
+
 
 main()
