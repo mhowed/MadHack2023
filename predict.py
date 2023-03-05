4594f673be3de74fb37eb04679a26e93ae2fb0f1
vrishankp <realvrishankp@gmail.com>
2023-03-04 21:55:59 -0600

    Reduce time to run application

--- predict.py
+++ predict.py
@@ -9,88 +9,108 @@ from polygon import RESTClient
 from polygon.rest import models
 import pandas as pd
 import yfinance as yahoo
+import os.path
+import json
 
 
-currtime = datetime.now().strftime('%Y-%m-%d')
-plt.style.use("dark_background")
+def prediction(ticker):
+    currtime = datetime.now().strftime('%Y-%m-%d')
+    print(currtime, type(currtime))
+    df = None
 
-df = pd.DataFrame(yahoo.download('AAPL', "2012-01-01", currtime))
-print(df)
-
-data = df.filter(["Close"])
-dataset = data.values
-
-
-train_split = .80
-
-train_len = math.ceil(len(dataset) * train_split)
-
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(dataset)
-
-train_data = scaled_data[0:train_len, :]
-
-x_train = []
-y_train = []
-
-days = 60
-
-for i in range(days, len(train_data)):
-    x_train.append(train_data[i-days:i, 0])
-    y_train.append(train_data[i,0])
+    if os.path.isfile(f"data/{ticker}.csv"):
+        dateOfFile = datetime.fromtimestamp(os.path.getmtime(f"data/{ticker}.csv")).strftime('%Y-%m-%d')
         
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+        if dateOfFile < currtime:
+            print("Fetching new Data")
+            pd.DataFrame(yahoo.download(ticker, "2012-01-01", currtime)).to_csv(f"data/{ticker}.csv")
+    else:
+        print("Getting new Data")
+        pd.DataFrame(yahoo.download(ticker, "2012-01-01", currtime)).to_csv(f"data/{ticker}.csv")
 
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1],1)))
-model.add(LSTM(50, return_sequences=False))
-model.add(Dense(25))
-#model.add(Dense(25))
-model.add(Dense(1))
+    df = pd.read_csv(f"data/{ticker}.csv")
 
-#compile
-model.compile(optimizer='adam', loss='mean_squared_error')
+    plt.style.use("dark_background")
 
-#fit using x,y,every piece of data,only 1 run
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+    data = df.filter(["Close"])
+    dataset = data.values
 
-test = scaled_data[train_len - days: , :]
-#Create the data set x_test and y_test
-x_test = []
-y_test = dataset[train_len:, :]
-for i in range(days, len(test)):
-    x_test.append(test[i-days:i, 0])
 
-#make it a numpy array
-x_test = np.array(x_test)
+    train_split = .80
 
-#reshape data
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    train_len = math.ceil(len(dataset) * train_split)
 
-#Get the models predicted price values
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
 
-#Get the root mean squared error (RMSE)
-rmse = np.sqrt(np.mean(predictions - y_test)**2)
-rmse
-print(rmse)
+    train_data = scaled_data[0:train_len, :]
 
-#get data we didnt touch
-valid = data[train_len:]
-#data that was predicted
-train = data[:train_len]
-valid['Predictions'] = predictions
+    x_train = []
+    y_train = []
 
-#plot data
-plt.figure(figsize=(16,8))
-plt.title('Model')
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.plot(train['Close'])
-plt.plot(valid[['Close', 'Predictions']])
-plt.legend(['Train', 'Val', 'Predictions'], loc='upper left')
-plt.savefig('image.png')
+    days = 60
 
-print(predictions[-1])
+    for i in range(days, len(train_data)):
+        x_train.append(train_data[i-days:i, 0])
+        y_train.append(train_data[i,0])
+            
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1],1)))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    #model.add(Dense(25))
+    model.add(Dense(1))
+
+    #compile
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    #fit using x,y,every piece of data,only 1 run
+    model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+    test = scaled_data[train_len - days: , :]
+    #Create the data set x_test and y_test
+    x_test = []
+    y_test = dataset[train_len:, :]
+    for i in range(days, len(test)):
+        x_test.append(test[i-days:i, 0])
+
+    #make it a numpy array
+    x_test = np.array(x_test)
+
+    #reshape data
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    #Get the models predicted price values
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+
+    #Get the root mean squared error (RMSE)
+    rmse = np.sqrt(np.mean(predictions - y_test)**2)
+    rmse
+    print(rmse)
+
+    #get data we didnt touch
+    valid = data[train_len:]
+    #data that was predicted
+    train = data[:train_len]
+    valid['Predictions'] = predictions
+
+    #plot data
+    plt.figure(figsize=(16,8))
+    plt.title('Model')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.plot(train['Close'])
+    plt.plot(valid[['Close', 'Predictions']])
+    plt.legend(['Train', 'Val', 'Predictions'], loc='upper left')
+    plt.savefig('image.png')
+
+    print(predictions[-1])
+
+def main():
+    prediction('AAPL')
+
+main()
